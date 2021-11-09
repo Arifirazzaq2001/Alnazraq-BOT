d18f6cc35eedd518694338b36f5a613d9c8ea7c0
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-09 21:01:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ let { create, Client } = require('@open-wa/wa-automate');
 let msgHandler = require('./main.js');
 let fs = require('fs');
 
+const options = () => {
+  const options = {
+    sessionId: "session",
+    authTimeout: 60,
+    blockCrashLogs: true,
+    disableSpins: true,
+    headless: true,
+    hostNotificationLang: 'PT_BR',
+    logConsole: false,
+    popup: true,
+    qrTimeout: 0,
+    useChrome: true,
+  }
+  return options
+}
+
 function startBot(conn = new Client) {
   conn.onMessage((async (message) => {
     msgHandler(conn, message);
