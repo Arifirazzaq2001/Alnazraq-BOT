cd17a56d892a3a3b47de4c8e75d271bcf38d352d
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-09 23:48:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+module.exports = msgHandler = async (master = new Client, message) => {
+  try {
+    let { id, from, sender, isGroupMsg, chat, caption, isMedia, mimetype, quotedMsg, quotedMsgObj, mentionedJidList } = message;
+    let { body } = message;
+    let { name, formattedTitle } = chat;
+  } catch (err) {
+    console.error(color('[ERROR]', 'red'), err);
+  }
+}
