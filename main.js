fff86edb126f35a08705b541e627ab6d2f4a46d3
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-10 11:52:47 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ module.exports = msgHandler = async (master = new Client, message) => {
     let { id, from, sender, isGroupMsg, chat, caption, isMedia, mimetype, quotedMsg, quotedMsgObj, mentionedJidList } = message;
     let { body } = message;
     let { name, formattedTitle } = chat;
+    let { pushname, verifiedName, formattedName } = sender;
+    pushname = pushname || verifiedName || formattedName;
+    let cmd = caption || body || '';
+    let command = cmd.toLowerCase().split(' ')[0] || '';
+    let args = cmd.split(' ');
+    
+    var prefix = /^[°•π÷×¶∆£¢€¥®™✓=|~!#$%^&./\\©^]/.test(command) ? command.match(/^[°•π÷×¶∆£¢€¥®™✓=|~!#$%^&./\\©^]/gi) : '-'
+    
   } catch (err) {
     console.error(color('[ERROR]', 'red'), err);
   }
